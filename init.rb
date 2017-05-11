f9fdca7c8e86c1f9a98d6132204be444e0e385c7
constXife <constxife@yandex.ru>
2017-05-11 22:31:45 +0700

    Fix permissions and redmine version.

--- init.rb
+++ init.rb
@@ -1,3 +1,6 @@
+# View hook
+require_dependency 'weekly_scheduler_sidebar_hook'
+
 Redmine::Plugin.register :weekly_scheduler do
   name 'Weekly Scheduler plugin'
   author 'constXife'
@@ -6,10 +9,10 @@ Redmine::Plugin.register :weekly_scheduler do
   url 'https://github.com/constxife/weekly_scheduler'
   author_url 'https://github.com/constxife'
 
-  requires_redmine version_or_higher: '3.0'
+  requires_redmine version_or_higher: '3.3'
 
   project_module :weekly_scheduler do
-    permission :view_schedule,   scheduler: :index
-    permission :manage_schedule, scheduler: :manage
+    permission :view_schedule,   weekly_schedules: :show, read: true
+    permission :manage_schedule, weekly_schedules: [:new, :edit, :destroy, :update], require: :loggedin
   end
 end
