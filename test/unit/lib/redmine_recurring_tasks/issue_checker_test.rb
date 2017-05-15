5c4ec24b08c32a53e156e697d24a94803f3f5428
constXife <constxife@yandex.ru>
2017-05-15 13:58:09 +0700

    Add fixtures to tests.

--- test/unit/lib/redmine_recurring_tasks/issue_checker_test.rb
+++ test/unit/lib/redmine_recurring_tasks/issue_checker_test.rb
@@ -2,6 +2,17 @@ require File.expand_path('../../../../test_helper', __FILE__)
 
 module RedmineRecurringTasks
   class IssueCheckerTest < ActiveSupport::TestCase
+    fixtures :projects, :users, :email_addresses, :user_preferences, :members, :member_roles, :roles,
+             :groups_users,
+             :trackers, :projects_trackers,
+             :enabled_modules,
+             :versions,
+             :issue_statuses, :issue_categories, :issue_relations, :workflows,
+             :enumerations,
+             :issues, :journals, :journal_details,
+             :custom_fields, :custom_fields_projects, :custom_fields_trackers, :custom_values,
+             :time_entries
+
     def test_schedules_return_schedule
       week_day = Time.now.strftime('%A').downcase
 
