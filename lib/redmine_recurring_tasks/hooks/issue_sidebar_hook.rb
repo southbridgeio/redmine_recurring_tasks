922f77cac2dd903f46a388a31e864fe5fed612ad
constXife <constxife@yandex.ru>
2017-05-15 12:44:29 +0700

    Add small doc to hook class.

--- lib/redmine_recurring_tasks/hooks/issue_sidebar_hook.rb
+++ lib/redmine_recurring_tasks/hooks/issue_sidebar_hook.rb
@@ -1,5 +1,6 @@
 module RedmineRecurringTasks
   module Hooks
+    # Issue sidebar hook for show weekly schedule on issue page
     class IssueSidebarHook < Redmine::Hook::ViewListener
       render_on :view_issues_sidebar_planning_bottom, partial: 'weekly_schedules/issues/sidebar'
     end
