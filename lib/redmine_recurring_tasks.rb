70fe817376d7a2cc7cda3d84c0b220ad33c75a62
constXife <constxife@yandex.ru>
2018-03-19 22:52:59 +0700

    Refactor self association

--- lib/redmine_recurring_tasks.rb
+++ lib/redmine_recurring_tasks.rb
@@ -4,7 +4,7 @@ module RedmineRecurringTasks
     default_associations = ['parent', 'project', 'tracker', 'status', 'author', 'assigned_to', 'fixed_version',
                            'priority', 'category', 'journals', 'visible_journals', 'time_entries', 'changesets',
                            'relations_from', 'relations_to', 'attachments', 'custom_values', 'watchers',
-                           'watcher_users', 'weekly_schedule']
+                           'watcher_users', 'recurring_task']
 
     Issue.reflections.keys.dup - default_associations
   end
