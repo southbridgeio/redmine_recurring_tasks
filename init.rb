28cdd73237f2b64c84908eeded526c9ef176a048
Vladislav Yashin <v.yashin.work@gmail.com>
2018-04-05 11:01:41 +0300

    Bump version [skip ci]

--- init.rb
+++ init.rb
@@ -14,7 +14,7 @@ Redmine::Plugin.register :redmine_recurring_tasks do
   name 'Redmine Recurring Tasks'
   author 'Southbridge'
   description 'Plugin for creating scheduled tasks from template'
-  version '0.2.1'
+  version '0.2.2'
   url 'https://github.com/centosadmin/redmine_recurring_tasks'
   author_url 'https://github.com/centosadmin'
 
